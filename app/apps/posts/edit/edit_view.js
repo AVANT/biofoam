73af49b57c621c627defb1696165abd1097bb71a
B. Cedric Cogell <cedric.cogell@gmail.com>
2013-09-22 22:36:41 -0400

    fixes the problem where errors stack and dont get cleared from the edit view

--- app/apps/posts/edit/edit_view.js
+++ app/apps/posts/edit/edit_view.js
@@ -22,12 +22,25 @@ define(function(require){
       },
 
       onFormDataInvalid: function(errors){
-        var self = this;
+        var $view = this.$el;
+
+        var clearFormErrors = function(){
+          var $form = $view.find('form');
+          $form.find('.help-inline.error').each(function(){
+            $(this).remove();
+          });
+          $form.find('.control-group.error').each(function(){
+            $(this).removeClass('error');
+          });
+        };
+
         var markErrors = function(value, key){
-          var $controlGroup = self.$el.find('#post-' + key).parent();
+          var $controlGroup = $view.find('#post-' + key).parent();
           var $errorEl = $('<span>', {class: 'help-inline error', text: value});
           $controlGroup.append($errorEl).addClass('error');
         };
+
+        clearFormErrors();
         _.each(errors, markErrors);
       }
     });
