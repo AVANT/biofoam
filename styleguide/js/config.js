39d75b477cdf7a6c7c4c25dcc7b20bf9074311d0
B. Cedric Cogell <cedric.cogell@gmail.com>
2013-11-24 18:40:35 -0500

    creatly wires up styleguide creator and tests functionality

--- styleguide/js/config.js
+++ styleguide/js/config.js
@@ -1,5 +1,5 @@
 define({
-	css_path: 'http://localhost:8000/css/main.css',
+	css_path: 'http://localhost:8000/main.css',
 	// This css path is used as a default by the Kalei project it self.
 	//css_path: window.location.protocol + '//' + window.location.hostname + (window.location.port === '' ? '' : ':'+ window.location.port) + window.location.pathname +  'css/styles.css',
 	// You can configure any path by just deleteing the one above and uncommenting the one below to point at your css directory
