dcb3871dc2cb6ff8b2f04b765a1cfd148f908314
B. Cedric Cogell <cedric.cogell@gmail.com>
2014-01-13 12:01:52 -0500

    fixes a few minor masonry problems that we saw last night

--- app/apps/posts/list/views/posts.js
+++ app/apps/posts/list/views/posts.js
@@ -9,15 +9,22 @@ return Moonrakr.module('Posts.List', function(List){
     tagName: 'div',
     className: 'posts js-masonry',
     itemView: List.Post,
-    onShow: function(){
+    onRender: function(){
       var masonEl = this.$el[0];
       console.log('masonry container el: ', masonEl);
-      var msnry = new Masonry( masonEl );
+      this.msnry = new Masonry( masonEl );
       //// todo: mansonry loads broken, I think it doesnt have the images loaded at the time of init
       //// could fix this by using imagesLoaded as suggested on the FAQ page of masonry
       // imagesLoaded( container, function() {
       //   msnry.layout();
       // });
+    },
+    onShow:function(){
+      var that = this;
+      // really shitty fix for this problem
+      setTimeout(function(){
+        that.msnry.layout();
+      }, 100)
     }
   });
 
