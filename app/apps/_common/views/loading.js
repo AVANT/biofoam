93041dcf17b204b343013cdd86931c0f2ce70763
Sam Berman <sam.berman@gmail.com>
2014-01-23 14:54:03 -0500

    styles loading page & fixes newsletter coloring bug on submission

--- app/apps/_common/views/loading.js
+++ app/apps/_common/views/loading.js
@@ -11,28 +11,28 @@ return Moonrakr.module("Common.Views", function(Views){
 
     serializeData: function(){
       return {
-        title: this.options.title || "Loading Data",
-        message: this.options.message || "Please wait, data is loading"
+        // title: this.options.title || "Loading Data",
+        // message: this.options.message || "Please wait, data is loading"
       };
     },
 
     onShow: function(){
       var opts = {
-        lines: 17, // The number of lines to draw
-        length: 30, // The length of each line
+        lines: 10, // The number of lines to draw
+        length: 2, // The length of each line
         width: 2, // The line thickness
-        radius: 54, // The radius of the inner circle
-        corners: 0, // Corner roundness (0..1)
+        radius: 7, // The radius of the inner circle
+        corners: 1, // Corner roundness (0..1)
         rotate: 0, // The rotation offset
         direction: 1, // 1: clockwise, -1: counterclockwise
-        color: '#000', // #rgb or #rrggbb or array of colors
+        color: '#333333', // #rgb or #rrggbb or array of colors
         speed: 0.5, // Rounds per second
-        trail: 95, // Afterglow percentage
+        trail: 50, // Afterglow percentage
         shadow: false, // Whether to render a shadow
         hwaccel: false, // Whether to use hardware acceleration
         className: 'spinner', // The CSS class to assign to the spinner
         zIndex: 2e9, // The z-index (defaults to 2000000000)
-        top: 'auto', // Top position relative to parent in px
+        top: 175, // Top position relative to parent in px
         left: 'auto' // Left position relative to parent in px
       };
       var target = document.getElementById('spinner');
