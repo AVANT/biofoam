031495c51771b0ffaccace7dcce986314bc3fd82
B. Cedric Cogell <cedric.cogell@gmail.com>
2014-01-28 23:23:18 -0500

    impliments facebook share and twitter share

--- app/apps/posts/show/views/post.js
+++ app/apps/posts/show/views/post.js
@@ -16,7 +16,61 @@ return Moonrakr.module('Posts.Show', function(Show){
       getHeaderImageUrl: function(){
         // return this.headerImage.filelink;
         return this.media[0].filelink;
+      },
+
+      facebookURL: function  () {
+
+        var baseUrl = 'https://www.facebook.com/dialog/feed?';
+        // var baseUrl = 'https://www.facebook.com/sharer/sharer.php?';
+
+        var appId = 'app_id=630547413679417';
+        var display = '&display=popup';
+        var caption = '&caption=' + encodeURIComponent(this.excerpt);
+        var link = '&link=' + encodeURIComponent(window.location.origin); // + /media/ + this.slug;
+        var redirect_uri = '&redirect_uri=' + link;
+        var picture = '&picture=' + encodeURIComponent(this.headerImageUrl);
+
+        var toReturn = baseUrl + appId + display + caption + link + redirect_uri + picture;
+
+        console.log('facebook', toReturn);
+
+        // return toReturn;
+        return toReturn;
+
+      },
+
+      twitterURL: function  () {
+
+        // 'http://twitter.com/share?
+        // text=text goes here
+        // &url=http://url goes here
+        // &hashtags=hashtag1,hashtag2,hashtag3'
+
+        var concatTitle = function (t) {
+
+          if( t.length > 100 ){
+            return t.substring(1, 100) + '...';
+          }
+
+          return t;
+
+        }(this.title)
+
+        var baseUrl = 'http://twitter.com/share?';
+
+        // var text = 'text={{ concatTitle }}';
+        var url = '&url=' + encodeURIComponent(window.location.origin); // + /media/ + this.slug;
+        var hashtags = '&hashtags=vvvnt';
+
+        var toReturn = baseUrl + url + hashtags;
+
+        console.log('twitter', toReturn);
+
+        // return toReturn;
+        return toReturn;
+
       }
+
     }
   });
 
