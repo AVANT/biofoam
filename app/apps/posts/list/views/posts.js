387c4a34af3ac416f3dd8f5f48366854ba3b50c9
B. Cedric Cogell <cedric.cogell@gmail.com>
2014-02-09 16:54:20 -0500

    cleans up posts.js view

--- app/apps/posts/list/views/posts.js
+++ app/apps/posts/list/views/posts.js
@@ -1,7 +1,6 @@
 require('app');
 require('handlebars');
-// var Masonry = require('masonry');
-window.salvattore = require('salvattore');
+var salvattore = require('salvattore');
 require('apps/posts/list/views/post');
 
 return Moonrakr.module('Posts.List', function(List){
@@ -13,41 +12,6 @@ return Moonrakr.module('Posts.List', function(List){
     onShow:function(){
       this.$el.attr('data-columns', '');
       salvattore.register_grid( this.$el[0] );
-    },
-
-    layoutHandler: function(msnryInstance, context){
-      context.centerThisInParent( msnryInstance );
-    },
-
-    centerThisInParent: function(msnryInstance){
-      var wWidth = $(window).width();
-
-      var setPaddingTo;
-
-      // if the window be smallz - using smallest breakpoint 768px
-      if ( wWidth <= 768 ){
-        setPaddingTo = 0;
-      } else {
-        var pWidth = this.$el.parent().parent().width(); // #header element
-        var mWidth = this.getMasonryWidth(msnryInstance);
-        setPaddingTo = parseInt( (pWidth - mWidth) / 2 );
-      }
-
-      this.$el.parent().css('padding-left', setPaddingTo);
-    },
-
-    getMasonryWidth:function (msnryInstance) {
-
-      var width = 0;
-
-      msnryInstance.items.some(function(e) {
-        if ( e.position.y > 0 ) return true;
-        var size = e.size;
-        width += size.borderLeftWidth + size.borderRightWidth + size.marginLeft  + size.marginRight + size.width + size.paddingLeft + size.paddingRight;
-        width += msnryInstance.gutter;
-      });
-
-      return width -= msnryInstance.gutter;
     }
 
   });
