b4ef9cadf11896db783bab1937e921dccf052d80
Sam Berman <sam.berman@gmail.com>
2014-02-17 14:16:26 -0500

    gallery changes, other small tweaks

--- app/apps/posts/show/show_controller.js
+++ app/apps/posts/show/show_controller.js
@@ -94,6 +94,34 @@ return Moonrakr.module('Posts.Show', function(Show){
       });
       // end header image fade out
 
+         ///////////////////////
+        // start carousel js //
+       ///////////////////////
+      $('#myCarousel').carousel({
+          interval: 4000
+      });
+
+      // handles the carousel thumbnails
+      $('[id^=carousel-selector-]').click( function(){
+        var id_selector = $(this).attr("id");
+        var id = id_selector.substr(id_selector.length -1);
+        id = parseInt(id);
+        $('#myCarousel').carousel(id);
+        $('[id^=carousel-selector-]').removeClass('selected');
+        $(this).addClass('selected');
+      });
+
+      // when the carousel slides, auto update
+      $('#myCarousel').on('slid', function (e) {
+        var id = $('.item.active').data('slide-number');
+        id = parseInt(id);
+        $('[id^=carousel-selector-]').removeClass('selected');
+        $('[id^=carousel-selector-'+id+']').addClass('selected');
+      });
+        /////////////////////
+       // end carousel js //
+      /////////////////////
+      
     }
   };
 
