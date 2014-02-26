ad34c028186fe9068160365df2fe7d5d784bbb18
B. Cedric Cogell <cedric.cogell@gmail.com>
2014-02-25 21:54:31 -0500

    version locks backbone

--- test/spec/app/app.spec.js
+++ test/spec/app/app.spec.js
@@ -2,14 +2,21 @@ define(function(require){
 
   require('app');
 
+  describe('App Dependencies', function(){
+    it('should have backbone defined', function () {
+      expect(Backbone).to.be.an('Object');
+    })
+  });
+
   describe('Moonrakr App', function(){
-    it('should exists', function(){
+
+    it('should exist', function(){
       expect(Moonrakr).to.be.an('object');
     });
 
     it('should have a config object with an "api" key', function(){
       expect( Moonrakr.Config ).to.be.an('object');
-      expect( Moonrakr.Config.api ).to.be.equal('cheruisibesares.com');
+      expect( Moonrakr.Config.api ).to.be.equal('/api');
     });
 
   });
