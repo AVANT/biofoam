c619dd4318eba74f174a4deab7963c72ea3c5b1f
B. Cedric Cogell <cedric.cogell@gmail.com>
2015-02-24 20:43:03 -0500

    small changes to footer view and template

--- _src/apps/footer/show/views/footer.js
+++ _src/apps/footer/show/views/footer.js
@@ -4,93 +4,85 @@ require('handlebars');
 
 var _footer = require('text!apps/footer/show/templates/footer.html');
 
-// return Moonrakr.module('Footer.Show', function(Show){
-  // Show.Footer = Marionette.CompositeView.extend({
-  return Marionette.CompositeView.extend({
-    template: Handlebars.compile( _footer ),
-    tagName: 'div',
-    className: 'footer',
-    ui: {
-      message: '.js-message'
-    },
-    events:{
-      'click .js-submit': 'submitClicked',
-      'click .js-about': 'aboutClicked',
-      'click .js-privacy': 'privacyClicked',
-      'click .js-sponsorship': 'sponsorshipClicked'
-    },
-    submitClicked: function(e){
-      e.preventDefault();
-      // clear message box
-      this.clearMessage();
-      // add a validation step
-      if ( this.validateInput() ){
-        this.register();
-      }
-    },
+return Marionette.CompositeView.extend({
+  template: Handlebars.compile( _footer ),
+  tagName: 'div',
+  className: 'footer',
 
-    validateInput:function(){
-      // validate that the input is actually an email
-      return true;
-    },
+  ui: {
+    message: '.js-message'
+  },
 
-    register:function(){
-      var that = this;
-      var $form = this.$el.find('form');
-      $.ajax({
-        type: $form.attr('method'),
-        url: $form.attr('action'),
-        data: $form.serialize(),
-        cache       : false,
-        dataType    : 'json',
-        contentType: 'application/json; charset=utf-8',
-        error       : that.ajaxErrorHandler,
-        success     : function(data) {
-          if (data.result != 'success') {
-              that.chimpErrorHandler( data );
-          } else {
-              that.successHandler( data );
-          }
+  events:{
+    'click .js-submit': 'submitClicked',
+    'click .js-about': 'aboutClicked',
+    'click .js-privacy': 'privacyClicked',
+    'click .js-sponsorship': 'sponsorshipClicked'
+  },
+
+  submitClicked: function(e){
+    e.preventDefault();
+    this.clearMessage();
+    if ( this.validateInput() ){
+      this.register();
+    }
+  },
+
+  validateInput: function () {
+    // validate that the input is actually an email
+    return true;
+  },
+
+  register: function () {
+    var that = this;
+    var $form = this.$el.find('form');
+    $.ajax({
+      type: $form.attr('method'),
+      url: $form.attr('action'),
+      data: $form.serialize(),
+      cache: false,
+      dataType: 'json',
+      contentType: 'application/json; charset=utf-8',
+      error: that.ajaxErrorHandler,
+      success: function(data) {
+        if (data.result != 'success') {
+            that.chimpErrorHandler( data );
+        } else {
+            that.successHandler( data );
         }
-      });
-    },
+      }
+    });
+  },
 
-    clearMessage: function(){
-      this.ui.message.html('');
-    },
+  clearMessage: function(){
+    this.ui.message.html('');
+  },
 
-    ajaxErrorHandler:function(err){
-      this.ui.message.html('Could not connect to the registration server. Please try again later.');
-    },
+  ajaxErrorHandler:function(err){
+    this.ui.message.html('Could not connect to the registration server. Please try again later.');
+  },
 
-    chimpErrorHandler:function(data){
-      // console.log('from mail chimp:', data);
-      this.ui.message.html( data.msg );
-    },
+  chimpErrorHandler:function(data){
+    // console.log('from mail chimp:', data);
+    this.ui.message.html( data.msg );
+  },
 
-    successHandler:function(data){
-      this.ui.message.html('Thanks for signing up.');
-    },
+  successHandler:function(data){
+    this.ui.message.html('Thanks for signing up.');
+  },
 
-    aboutClicked:function(e){
-      e.preventDefault()
-      this.trigger('about:clicked')
-    },
-    privacyClicked:function(e){
-      e.preventDefault()
-      this.trigger('privacy:clicked')
-    },
-    sponsorshipClicked:function(e){
-      e.preventDefault()
-      this.trigger('sponsorship:clicked')
-    },
+  aboutClicked:function(e){
+    e.preventDefault()
+    this.trigger('about:clicked')
+  },
 
+  privacyClicked:function(e){
+    e.preventDefault()
+    this.trigger('privacy:clicked')
+  },
 
-
-
-
-
-
-  // });
-
+  sponsorshipClicked:function(e){
+    e.preventDefault()
+    this.trigger('sponsorship:clicked')
+  }
 });
